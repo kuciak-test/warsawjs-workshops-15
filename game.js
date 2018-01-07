15a1837e23462f6d8f2ac0802118ed922c554974
kuciak-test <35187179+kuciak-test@users.noreply.github.com>
2018-01-07 13:16:07 +0100

    krok 6 - macierz

--- game.js
+++ game.js
@@ -34,10 +34,76 @@ document.addEventListener('DOMContentLoaded', function(){
 
        this.removeEventListener('click', fieldClickHandler);
 
+       /*
        if (emptyFields === 0) {
            alert('End of the Game!');
-       }
+       }*/
+       checkWinner();
    }
 
+   function checkWinner() {
+       var fields = document.querySelectorAll('.board > div');
+
+    /*
+
+      +---+---+---+
+      | 0 | 1 | 2 |
+      +---+---+---+
+      | 3 | 4 | 5 |
+      +---+---+---+
+      | 6 | 7 | 8 |
+      +---+---+---+
+
+   */
+       var macierz = [
+           [fields[0].className, fields[1].className, fields[2].className],
+           [fields[3].className, fields[4].className, fields[5].className],
+           [fields[6].className, fields[7].className, fields[8].className]
+       ];
+
+       for (idx = 0; idx < 3; idx++) {
+           //console.log('['+macierz[idx][0]+','+macierz[idx][1]+','+macierz[idx][2]+']');
+           if (macierz[idx][0] === macierz[idx][1] &&
+               macierz[idx][0] === macierz[idx][2] &&
+               macierz[idx][0] != '') {
+
+               alert(macierz[idx][0] + ' wins!');
+               return;
+           }
+       }
+
+       for (idx = 0; idx < 3; idx++) {
+           console.log('['+macierz[0][idx]+','+macierz[1][idx]+','+macierz[2][idx]+']');
+           if (macierz[0][idx] === macierz[1][idx] &&
+               macierz[0][idx] === macierz[2][idx] &&
+               macierz[0][idx] != '') {
+
+               alert(macierz[0][idx] + ' wins!');
+               return;
+           }
+       }
+
+       // przekątna pierwsza
+       if (macierz[0][0] === macierz[1][1] &&
+           macierz[0][0] === macierz[2][2] &&
+           macierz[0][0] != '') {
+
+           alert(macierz[0][0] + ' wins!');
+       }
+
+       // przekątna druga
+       if (macierz[0][2] === macierz[1][1] &&
+           macierz[0][2] === macierz[2][0] &&
+           macierz[0][2] != '') {
+
+           alert(macierz[0][2] + ' wins!');
+       }
+
+       if (emptyFields === 0) {
+           alert('Tie');
+           return;
+       }
+
+   }
 
 });

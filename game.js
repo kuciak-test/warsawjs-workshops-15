6be9abe4fb8f7385f8be9750af9287cadd3df82d
kuciak-test <35187179+kuciak-test@users.noreply.github.com>
2018-01-07 11:19:48 +0100

    krok 1

---
+++ game.js
@@ -0,0 +1,21 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+   initGame();
+
+   function initGame() {
+
+       var fields = document.querySelectorAll('.board > div');
+
+       //fields.forEach(field => field.addEventListener('click', fieldClickHandler));
+       fields.forEach(field => field.addEventListener('click', fieldClickHandler));
+   }
+
+   function fieldClickHandler(){
+
+       console.log("Hello", this);
+   }
+
+
+});
